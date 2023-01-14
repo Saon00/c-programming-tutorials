07f43222e04c9da82b334f3597cacb12049f46b9
Saon00 <57843701+Saon00@users.noreply.github.com>
2023-01-14 15:49:23 +0600

    area of a circle

---
+++ hsc_ict_5th_chapter/areaofcircle.c
@@ -0,0 +1,18 @@
+/*
+Author
+Md. Saon Sikder
+BSc. in Computer Science & Engineering
+Faridpur Engineering College
+Youtube-Facebook-Instagram ID: BlackOsRa
+*/
+
+#include <stdio.h>
+int main()
+{
+    float pi = 3.14, r;
+    printf("Enter the value of r: ");
+    scanf("%f", &r);
+    float area = pi * r * r;
+    printf("The area is: %f", area);
+    return 0;
+}
